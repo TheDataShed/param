99bc2f570788cc36dd828fe17dbd28bb1ec3fc0a
Will Johnson <willjcjohnson@gmail.com>
2018-07-02 01:55:15 +0100

    Added Complete Command

---
+++ cmd/completion.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var completionCmd = &cobra.Command{
+	Use:   "completion (bash|zsh)",
+	Short: "Generates shell completion scripts",
+	Long: `Output shell completion code for the specified shell (bash or zsh).
+The shell code must be evaluated to provide interactive completion of param commands.
+This can be done by sourcing it from ~/.bashrc
+
+Examples:
+    # Installing bash completion
+    printf "
+    # param shell completion
+    source <(param completion bash)
+    " >> $HOME/.bashrc
+    source $HOME/.bashrc`,
+	Args:      cobra.OnlyValidArgs,
+	ValidArgs: []string{"bash", "zsh"},
+	Run: func(cmd *cobra.Command, args []string) {
+		if args[0] == "bash" {
+			RootCmd.GenBashCompletion(os.Stdout)
+		} else if args[0] == "zsh" {
+			RootCmd.GenZshCompletion(os.Stdout)
+		}
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(completionCmd)
+}
